064750860ae0b446ef1e1284e2ea79ba6804c60b
Henry <142275543+Henry-Vieira@users.noreply.github.com>
2024-07-10 11:08:03 -0300

    Add files via upload

---
+++ 1006.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double A, B, C;
+
+    cin >> A >> B >> C;
+
+    double media = (A * 2 + B * 3 + C * 5) / 10;
+
+    cout << "MEDIA = " << fixed << setprecision(1) << media << endl;
+
+    return 0;
+}
